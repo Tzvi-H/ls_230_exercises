a5619101f4c6307fcd93c6a01d50284bece5f40f
Tzvi-H <16guitar@gmail.com>
2020-04-27 12:20:49 -0500

    solution 2

--- events_async_programming/article_highlighter.js
+++ events_async_programming/article_highlighter.js
@@ -1,29 +1,21 @@
-document.addEventListener('DOMContentLoaded', e => {
-  document.body.addEventListener('click', addHighlight);
+document.addEventListener('click', e => {
+  removeHighlight();
+  highlightElement(e.target);
 })
 
-function removeHighlight() {
-  let highlighted = document.querySelector('.highlight');
-  if (highlighted) {
-    highlighted.classList.remove('highlight');
+function highlightElement(target) {
+  let node;
+  if (target.tagName === 'A') {
+    node = document.querySelector(target.hash)
+  } else if (target.tagName === 'ARTICLE' || target.parentElement.tagName === 'ARTICLE') {
+    node = target.tagName === 'ARTICLE' ? target : target.parentElement;
+  } else {
+    node = document.querySelector('main');
   }
-}
-
-function addHighlight(event) {
-  event.stopPropagation();
-  removeHighlight();
-  let node = calculateNode(event.target);
   node.classList.add('highlight');
 }
 
-function calculateNode(target) {
-  let node;
-  if (target.tagName === 'A') {
-    node = document.querySelector(target.getAttribute('href'));
-  } else if (target.parentNode.tagName === 'ARTICLE') {
-    node = target.parentNode;
-  } else {
-    node = document.querySelector('main');;
-  }
-  return node;
+function removeHighlight() {
+  let elements = document.querySelectorAll('.highlight');
+  [].forEach.call(elements, element => element.classList.remove('highlight'));
 }
